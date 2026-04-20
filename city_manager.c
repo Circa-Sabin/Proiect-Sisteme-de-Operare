a1ef6a843db3ebce73293e8073fd3b65ac243136
Circa Sabin <sabin-casian.circa@student.upt.ro>
2026-04-20 13:27:29 +0300

    Am adaugat si o parte din functiile de baza

--- city_manager.c
+++ city_manager.c
@@ -6,6 +6,8 @@
 #include <sys/stat.h>
 #include <string.h>
 #include <dirent.h>
+#include <time.h>
+
 
 #define MAX_NAME 50
 #define MAX_CAT 30
@@ -86,13 +88,192 @@ void get_argv(int argc, char *argv[]) {
 
 // logica comenzilor
 
-void do_district() {};
+void do_district() {
 
-void do_add(){};
+    mkdir(district, 0750);
+    chmod(district, 0750);
 
-void do_list(){};
+    char filepath[150];
 
-void do_view(){};
+    // partea pentru reports.dat
+
+    sprintf(filepath, "%s/reports.dat", district);
+    int fd_reports = open(filepath, O_CREAT | O_RDWR, 0664);
+    if (fd_reports != -1) {
+        close(fd_reports);
+    }
+
+    chmod(filepath, 0664);
+
+    // partea pentru district.cfg
+
+    sprintf(filepath, "%s/district.cfg", district);
+    int fd_cfg = open(filepath, O_CREAT | O_RDWR, 0640);
+    if (fd_cfg != -1) {
+        close(fd_cfg);
+    }
+    chmod(filepath, 0640);
+
+    // partea pentru logged_district
+
+    sprintf(filepath, "%s/logged_district", district);
+    int fd_log = open(filepath, O_CREAT | O_RDWR, 0644);
+    if (fd_log != -1) {
+        close(fd_log);
+    }
+    chmod(filepath, 0644);
+};
+
+void do_add(){
+
+    Report report;
+
+    memset(&report, 0, sizeof(Report)); // curatare memorie 
+
+    printf("Introduceti latitudinea X: ");
+    scanf("%f", &report.latitude);
+
+    printf("Introduceti longitudinea Y: ");
+    scanf("%f", &report.longitude);
+
+    printf("Introduceti categoria problemei: ");
+    scanf("%s", report.category);
+
+    printf("Introduceti severitatea (1 = minor, 2 = moderate, 3 = critical): ");
+    scanf("%d", &report.severity);
+
+    printf("Introduceti o descriere a problemei: ");
+    scanf(" %[^\n]", report.description);
+
+    strcpy(report.inspector_name, user);
+    report.timestamp = time(NULL);
+
+    char filepath[150];
+    sprintf(filepath, "%s/reports.dat", district);
+    int fd = open(filepath, O_WRONLY | O_APPEND); // O_APPEND pentru a adauga la finalul fisierului
+
+    if(fd == -1) {
+        perror("Eroare la deschiderea fisierului reports.dat!\n");
+        return;
+    }
+
+    int marime_fisier = lseek(fd, 0, SEEK_END); // lseek arata marimea totala a fisierului in bytes
+    report.id = marime_fisier / sizeof(Report) + 1; 
+
+    write(fd, &report, sizeof(Report));
+    close(fd);
+};
+
+void printeaza_bucata(int cifra) {
+    if (cifra == 0) printf("---");
+    else if (cifra == 1) printf("--x");
+    else if (cifra == 2) printf("-w-");
+    else if (cifra == 3) printf("-wx");
+    else if (cifra == 4) printf("r--");
+    else if (cifra == 5) printf("r-x");
+    else if (cifra == 6) printf("rw-");
+    else if (cifra == 7) printf("rwx");
+}
+
+void print_permissions(mode_t mode) {
+
+    int perm_user = (mode / 64) % 8; // extrage permisiunile pentru user
+    int perm_group = (mode / 8) % 8; // extrage perm
+    int perm_other = mode % 8; // extrage permisiunile pentru others
+
+    printeaza_bucata(perm_user);
+    printeaza_bucata(perm_group);
+    printeaza_bucata(perm_other);
+}
+
+void do_list(){
+
+    char filepath[150];
+    sprintf(filepath, "%s/reports.dat", district); // construieste calea
+
+    struct stat st;
+    if (stat(filepath, &st) == -1) {
+        perror("Nu am gasit fisierul reports.dat!\n");
+        return;
+    }
+
+    printf("\n INFO FISIER \n");
+    printf("Permisiuni: ");
+    print_permissions(st.st_mode);
+
+    printf("\nMarime: %ld bytes\n", st.st_size);
+    printf("Ultima modificare: %s", ctime(&st.st_mtime));
+    printf("\n\n");
+
+    int fd = open(filepath, O_RDONLY);
+    if (fd == -1) {
+        perror("Eroare la deschiderea fisierului reports.dat!\n");
+        return;
+    }
+
+    Report report;
+    int numar_rapoarte = 0;
+
+    printf("Lista Rapoarte \n");
+
+    while(read(fd, &report, sizeof(Report)) == sizeof(Report)) {
+        numar_rapoarte++;
+        printf("ID: %d | Inspector: %s | Severitate: %d\n", report.id, report.inspector_name, report.severity);
+        printf("GPS: %.2f, %.2f | Categoria: %s\n", report.latitude, report.longitude, report.category);
+        printf("Descriere: %s\n", report.description);
+        printf("\n");
+    }   
+
+    if(numar_rapoarte == 0) {
+        printf("Nu exista rapoarte in districtul s%s.\n", district);
+    }
+
+    close(fd);
+
+};
+
+void do_view(){
+
+    int target_id = atoi(variabila_extra); // converteste argumentul in int
+
+    char filepath[150];
+    sprintf(filepath, "%s/reports.dat", district); // construieste calea
+
+    int fd = open(filepath, O_RDONLY);
+    if (fd == -1) {
+        printf("Nu am gasit fisierul reports.dat in districtul %s!\n", district);
+        return;
+    }
+
+    Report report;
+
+    int raport_gasit = 0;
+
+    while (read(fd, &report, sizeof(Report)) == sizeof(Report)) {
+        
+        if(report.id == target_id){
+            raport_gasit = 1;
+        
+
+            printf("\n DETALII RAPORT ID %d\n", report.id);
+            printf("Inspector: %s\n", report.inspector_name);
+            printf("Data si ora: %s", ctime(&report.timestamp));
+            printf("GPS: %.2f, %.2f\n", report.latitude, report.longitude);
+            printf("Categoria: %s\n", report.category);
+            printf("Severitate: %d\n", report.severity);
+            printf("Descriere: %s\n", report.description);
+            printf("\n");
+            break;
+    
+        }
+    }
+
+    if (raport_gasit == 0){
+        printf("Nu am gasit raportul cu ID %d in districtul %s!\n", target_id, district);
+    }
+
+    close(fd);
+}
 
 void do_remove_report(){};
 
